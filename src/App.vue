0451e53d814124a7016f1fcca1a9839e9414a853
Nercus <48932939+Nercus@users.noreply.github.com>
2025-04-11 15:51:54 +0200

    feat: add reload fail save

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <Notivue v-slot="item">
     <Notification :item="item" />
   </Notivue>
+
   <div class="w-dvw h-dvh bg-base-100 overflow-hidden flex flex-col items-center justify-center">
     <RouterView />
   </div>
@@ -15,9 +16,54 @@ import { RouterView } from 'vue-router'
 const { connected } = useConnection()
 const router = useRouter()
 
-watch(connected, (val) => {
+watch(connected, (val, oldVal) => {
   if (val) {
+    push.success({
+      title: 'Connected',
+      message: 'You are now connected.',
+      duration: 5000,
+    })
     router.push({ name: 'Setup' })
+  }
+  if (!val && oldVal) {
+    push.error({
+      title: 'Disconnected',
+      message: 'You have been disconnected.',
+      duration: 5000,
+    })
+    router.push({ name: 'Home' })
+  }
+})
+
+window.addEventListener('beforeunload', (event) => {
+  if (import.meta.env.MODE === 'development') return
+  if (!connected.value) return
+  // Custom message may not display in modern browsers, but this will trigger a confirmation dialog
+  event.preventDefault()
+})
+
+function showWarning() {
+  push.warning({
+    title: 'Warning',
+    message: 'Refreshing the page will end the connection. Please use the browser\'s reload button instead, if you insist on refreshing.',
+    duration: 5000,
+  })
+}
+
+const showWarningDebounced = useDebounceFn(showWarning, 200, {
+  maxWait: 500,
+})
+
+document.addEventListener('keydown', (event) => {
+  if (import.meta.env.MODE === 'development') return
+  if (!connected.value) return
+  if ((event.ctrlKey || event.metaKey) && event.key === 'r') {
+    event.preventDefault()
+    showWarningDebounced()
+  }
+  if (event.key === 'F5') {
+    event.preventDefault()
+    showWarningDebounced()
   }
 })
 </script>
