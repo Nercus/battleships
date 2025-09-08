b421380a228ea54220cb7176c8cec658956977d5
Nercus <48932939+Nercus@users.noreply.github.com>
2025-09-08 22:02:44 +0200

    chore: merge incoming and outgoing

---
+++ src/views/Room.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-col justify-center items-center gap-4 h-full">
+    {{ room.roomId }}
+    <Button :disabled="room.roomId.value !== ''" @click="room.createRoom()">
+      Create Room
+    </Button>
+
+    <div>
+      <Textarea v-model="newRoomId" placeholder="Room ID" />
+    </div>
+    <Button :disabled="newRoomId === ''" @click="room.joinRoom(newRoomId)">
+      Join Room
+    </Button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const room = useRoom()
+
+const newRoomId = ref('')
+</script>
