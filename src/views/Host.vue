e57c079366f10df152d8f81e5e25d6d2a0a9c5a2
Nercus <48932939+Nercus@users.noreply.github.com>
2025-09-10 14:46:42 +0200

    fix: adjust responsive error on room creation page

--- src/views/Host.vue
+++ src/views/Host.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col justify-center items-center gap-4 p-4 lg:p-20 w-full max-w-lg lg:max-w-2xl">
-    <span class="flex flex-row justify-centers items-center gap-2 font-black text-xl tracking-wide">
+    <span class="flex md:flex-row flex-col justify-centers items-center gap-2 font-black text-xl tracking-wide">
       New Room
       <Input v-model="roomId" has-copy-button disabled :copy-format="generateURL" />
     </span>
