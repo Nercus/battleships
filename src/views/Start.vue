0a0fe3985c57681d4286a094b7aa6736ff38ef97
Nercus <48932939+Nercus@users.noreply.github.com>
2025-09-10 22:40:23 +0200

    chore: remove animated logo

--- src/views/Start.vue
+++ src/views/Start.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     class="flex flex-col justify-center items-center gap-4 lg:gap-10 p-4 lg:p-20 w-full max-w-lg overflow-visible">
-    <AnimatedLogo class="w-1/2" />
+    <img src="../assets/logo.svg" alt="Compass Logo" class="w-1/2">
     <h1 class="z-10 font-black text-xl tracking-wide">
       Battleships
     </h1>
