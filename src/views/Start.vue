61b8c5790b7c5312baaf64a02f6e61178b7f975c
Nercus <48932939+Nercus@users.noreply.github.com>
2025-10-20 13:39:00 +0200

    chore: add png format for logo and use on start page

--- src/views/Start.vue
+++ src/views/Start.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     class="flex flex-col justify-center items-center gap-2 lg:gap-4 p-4 lg:p-20 w-full max-w-lg overflow-visible">
-    <img src="../assets/logo.svg" alt="Compass Logo" class="w-1/2">
+    <img src="../assets/logo.png" alt="Compass Logo" class="w-1/2">
     <h1 class="z-10 font-black text-xl tracking-wide">
       Battleships
     </h1>
