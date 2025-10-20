30774c21dca8d4e2864ef5a5fd087507270a4565
Nercus <48932939+Nercus@users.noreply.github.com>
2025-10-20 14:51:28 +0200

    feat: add how to play info

---
+++ src/components/HowToPlay.vue
@@ -0,0 +1,34 @@
+<template>
+  <HoverCardRoot>
+    <HoverCardTrigger class="top-2 -right-2 absolute flex justify-center items-center bg-base-800 drop-shadow p-1 rounded-full size-6 translate-x-full">
+      <Icon class="bg-white size-full fluent--question-16-filled" />
+    </HoverCardTrigger>
+    <HoverCardPortal>
+      <HoverCardContent align="start" class="z-10 bg-white shadow-sm p-3 pl-7 border border-gray-300 rounded-xl max-w-sm">
+        <HoverCardArrow />
+        <h3 class="mb-2 font-semibold text-lg">
+          How to Play
+        </h3>
+        <ul class="list-disc">
+          <li>
+            Each player takes turns to attack a cell on their opponent's grid.
+          </li>
+          <li>
+            The goal is to sink all of your opponent's ships before they sink yours.
+          </li>
+          <li>
+            Click on a cell to aim at a position on your opponent's board.
+          </li>
+          <li>
+            Confirm your attack by double clicking the selected cell.
+          </li>
+          <li>
+            A hit is marked with <span class="inline-block"> <GameGridHitIndicator type="hit" :index="-1" board-title="" class="size-4" /></span>
+            and a miss with <span class="inline-block"> <GameGridHitIndicator type="miss" :index="-1" board-title="" class="size-4" /></span>.
+          </li>
+        </ul>
+        <p class="inline-flex gap-2 mb-2 text-sm" />
+      </HoverCardContent>
+    </HoverCardPortal>
+  </HoverCardRoot>
+</template>
--- src/views/PlayerTurn.vue
+++ src/views/PlayerTurn.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="flex md:flex-row flex-col justify-center items-center gap-2 md:gap-4 lg:gap-8 p-4 w-full h-full">
     <div class="relative background-grid aspect-square" style="width: min(70vw, 70vh, 40rem); height: min(70vw, 70vh, 40rem);">
+      <HowToPlay />
       <GameGridShipLayer v-model:layout="destroyedShips" class="absolute inset-0 w-[calc(100%-1px)] h-[calc(100%-1px)]" :color="opponentColor" :is-draggable="false" />
-      <GameGridHitLayer class="absolute inset-0 w-[calc(100%-1px)] h-[calc(100%-1px)]" :board="opponentBoardHitStates" title="Your turn (Double click to shoot!)" board-type="opponent" :class="{ 'pointer-events-none': attackBlocked }" @shoot="onShoot" />
+      <GameGridHitLayer class="absolute inset-0 w-[calc(100%-1px)] h-[calc(100%-1px)]" :board="opponentBoardHitStates" title="Your turn" board-type="opponent" :class="{ 'pointer-events-none': attackBlocked }" @shoot="onShoot" />
     </div>
     <div class="hidden lg:block relative background-grid opacity-70 h-1/5 lg:h-1/3 aspect-square pointer-events-none">
       <GameGridShipLayer v-model:layout="shipLayout" class="absolute inset-0 w-[calc(100%-1px)] h-[calc(100%-1px)]" :color="playerColor" :is-draggable="false" />
@@ -59,8 +60,8 @@ function onShoot(x: number, y: number) {
 .background-grid::before {
   content: '';
   background-size: calc(calc(100% - 2px) / 10) calc(calc(100% - 2px) / 10);
-  background-image: linear-gradient(to right, black 1px, transparent 1px),
-    linear-gradient(to bottom, black 1px, transparent 1px);
+  background-image:
+    linear-gradient(to right, black 1px, transparent 1px), linear-gradient(to bottom, black 1px, transparent 1px);
   height: calc(100% - 2px);
   width: calc(100% - 2px);
   position: absolute;
