bb2f03a2fe2894d8aa26721ce97b94ab6e9b54e7
Sebastian Wintersperger <s.wintersperger@me.com>
2026-01-13 22:59:29 +0100

    fix: number label alignment

--- src/components/grid/GridLabels.vue
+++ src/components/grid/GridLabels.vue
@@ -1,6 +1,6 @@
 <template>
-  <div :class="labelStyle({ orientation })">
-    <span v-for="(label, index) in labels" :key="index" class="block" :style="orientation === 'horizontal' ? `width: calc(10%);` : `height: calc(10%);`">
+  <div :class="labelBarStyle({ orientation })">
+    <span v-for="(label, index) in labels" :key="index" :class="labelStyle({ orientation })">
       {{ label }}
     </span>
   </div>
@@ -13,11 +13,20 @@ const props = defineProps<{
   orientation: 'horizontal' | 'vertical'
 }>()
 
-const labelStyle = cva('flex justify-between shadow-sm border border-base-200 rounded-full font-bold text-sm text-base-700/50 select-none', {
+const labelBarStyle = cva('flex justify-between shadow-sm border border-base-200 rounded-full font-bold text-sm text-base-700/50 select-none', {
   variants: {
     orientation: {
       horizontal: 'w-70 md:w-100 text-center my-px',
-      vertical: 'h-70 md:h-100 flex-col align-center mx-px text-center',
+      vertical: 'h-70 md:h-100 flex-col mx-px text-center',
+    },
+  },
+})
+
+const labelStyle = cva('flex justify-center items-center', {
+  variants: {
+    orientation: {
+      horizontal: 'w-7 md:w-10',
+      vertical: 'h-7 md:h-10',
     },
   },
 })
