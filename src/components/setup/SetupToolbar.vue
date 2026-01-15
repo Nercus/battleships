0e36a35cff5673f8490a7f779b210c6bea81544b
Nercus <48932939+Nercus@users.noreply.github.com>
2026-01-15 16:32:25 +0100

    fix: add maxlength attribute to player name input

--- src/components/setup/SetupToolbar.vue
+++ src/components/setup/SetupToolbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col items-end md:items-center gap-4 w-full max-w-xs md:max-w-md">
-    <CommonInput v-model="playerName" label="Enter your name" class="w-full" />
+    <CommonInput v-model="playerName" label="Enter your name" class="w-full" maxlength="12" />
     <SetupToolbarColorSelect v-model="playerColor" class="order-3 md:order-2 w-full md:w-fit" />
   </div>
 </template>
