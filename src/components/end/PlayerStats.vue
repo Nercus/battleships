5d5fd820d844341db93576a63d48db783e7eda57
Nercus <48932939+Nercus@users.noreply.github.com>
2026-01-22 15:00:02 +0100

    feat: add icons to stat cards

--- src/components/end/PlayerStats.vue
+++ src/components/end/PlayerStats.vue
@@ -1,10 +1,11 @@
 <template>
   <motion.div
     :initial="{ opacity: 0 }" :animate="{ opacity: 1 }" :transition="{
-      duration: 5,
+      duration: 3,
     }" class="landscape:max-lg:hidden block">
     <div class="gap-4 grid grid-cols-2 md:grid-cols-4 px-8">
       <div v-for="(stat, key) in stats" :key="key" class="flex flex-col justify-center items-center bg-base-100 shadow-shadow p-4 border-2">
+        <Icon :class="statVariants({ type: key })" class="size-10" />
         <h2 class="mb-2 font-medium text-lg text-center">
           {{ stat.label }}
         </h2>
@@ -17,6 +18,7 @@
 </template>
 
 <script setup lang="ts">
+import { cva } from 'class-variance-authority'
 import { motion } from 'motion-v'
 
 const { destroyedShips, opponentBoardHitStates } = useGame()
@@ -30,6 +32,17 @@ const misses = computed(() => opponentBoardHitStates.value.flat().flat().filter(
 const accuracy = computed(() => {
   const totalShots = hits.value + misses.value
   return totalShots === 0 ? 0 : Math.round((hits.value / totalShots) * 100)
+})
+
+const statVariants = cva('', {
+  variants: {
+    type: {
+      shipsDestroyed: 'text-primary fluent--vehicle-ship-16-filled',
+      hits: 'text-success fluent--target-16-filled',
+      misses: 'text-error fluent--circle-off-16-filled',
+      accuracy: 'text-warning fluent--arrow-move-inward-20-filled',
+    },
+  },
 })
 
 const stats = computed(() => ({
