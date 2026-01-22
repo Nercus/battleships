585592592a760afe013ccbe8c6d8fcb8eec9ec7a
Nercus <48932939+Nercus@users.noreply.github.com>
2026-01-22 15:51:50 +0100

    fix: remove mobile check from target shooting logic

--- src/components/grid/indicator/IndicatorGrid.vue
+++ src/components/grid/indicator/IndicatorGrid.vue
@@ -25,10 +25,6 @@ const flatBoard = computed(() => {
   return props.board?.flat() || Array.from({ length: 100 }).fill('none') as HitType[]
 })
 
-const isMobile = computed(() => {
-  return 'ontouchstart' in window
-})
-
 function shootTarget(hitType: HitType, index: number) {
   if (hitType !== 'none') return
   if (!playerTarget.value) return
@@ -44,7 +40,7 @@ function setTarget(hitType: HitType, index: number) {
   const x = Math.floor(index / 10)
   const y = index % 10
 
-  if (isMobile.value && playerTarget.value?.x === x && playerTarget.value?.y === y) {
+  if (playerTarget.value?.x === x && playerTarget.value?.y === y) {
     emit('shoot', x, y)
     return
   }
