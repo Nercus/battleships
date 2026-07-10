b4a7eaa03ac0c7f575aff8de76ba3cdf1dfe7b0a
Nercus <48932939+Nercus@users.noreply.github.com>
2026-07-10 14:44:45 +0200

    chore: adjust icon for setup info text

--- src/components/common/CommonInfoText.vue
+++ src/components/common/CommonInfoText.vue
@@ -1,10 +1,6 @@
 <template>
-  <span class="flex items-center gap-0.5 shadow-base-700 shadow-shadow p-0.5 border max-w-xs text-tiny text-base-700">
-    <Icon class="size-5 text-warning fluent--info-20-filled" />
+  <span class="flex items-center gap-1 shadow-base-700 shadow-shadow p-0.5 border max-w-xs text-tiny text-base-700">
+    <Icon class="size-5 text-warning fluent--question-circle-20-filled" />
     <slot />
   </span>
 </template>
-
-<script setup lang="ts">
-
-</script>
