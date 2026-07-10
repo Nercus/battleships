0206b6eefc72d207cf938c033bbc507d01aee060
Sebastian Wintersperger <s.wintersperger@me.com>
2026-07-10 23:49:37 +0200

    feat: rework 3d logo

--- src/components/logo/LogoScene.vue
+++ src/components/logo/LogoScene.vue
@@ -1,54 +1,75 @@
 <template>
-  <TresGroup ref="coinGroupRef">
-    <TresGroup v-for="(shape, index) in logoShapes" :key="`shape-${index}`">
-      <TresMesh :position="logoPosition" :scale="logoScale">
-        <TresExtrudeGeometry :args="[shape, { depth: 0.1, bevelEnabled: false }]" />
-        <TresMeshBasicMaterial color="white" />
+  <TresGroup ref="logoGroupRef">
+    <TresGroup :position="frontLogoPosition" :scale="logoScale">
+      <TresMesh v-for="(geometry, index) in frontLogoGeometries" :key="`front-logo-${index}`" :geometry="geometry">
+        <TresMeshBasicMaterial color="#f5f5f5" :side="DoubleSide" />
       </TresMesh>
+    </TresGroup>
 
-      <TresLineSegments :position="logoPosition" :scale="logoScale">
-        <TresEdgesGeometry :args="[new ExtrudeGeometry(shape, { depth: 0.1, bevelEnabled: false })]" />
-        <TresLineBasicMaterial color="black" />
-      </TresLineSegments>
+    <TresGroup :position="middleLogoPosition" :scale="logoScale">
+      <TresMesh v-for="(geometry, index) in middleLogoGeometries" :key="`middle-logo-${index}`" :geometry="geometry">
+        <TresMeshBasicMaterial color="#1a1a1a" :side="DoubleSide" />
+      </TresMesh>
+    </TresGroup>
+
+    <TresGroup :position="backLogoPosition" :scale="logoScale">
+      <TresMesh v-for="(geometry, index) in backLogoGeometries" :key="`back-logo-${index}`" :geometry="geometry">
+        <TresMeshBasicMaterial color="#f5f5f5" :side="DoubleSide" />
+      </TresMesh>
     </TresGroup>
   </TresGroup>
 </template>
 
 <script setup lang="ts">
 import { useLoader, useLoop } from '@tresjs/core'
-import { ExtrudeGeometry, Vector3 } from 'three'
+import { DoubleSide, ExtrudeGeometry, Vector3 } from 'three'
 import { SVGLoader } from 'three/examples/jsm/Addons.js'
 
 const { state: svgData } = useLoader(SVGLoader, '/assets/logo.svg')
+const whiteLayerDepth = 0.045
+const blackLayerDepth = 0.03
 const logoPosition = new Vector3(-0.75, 0.75, -0.05)
 const logoScale = new Vector3(0.003, -0.003, 1)
+const frontLogoPosition = new Vector3(logoPosition.x, logoPosition.y, blackLayerDepth / 2)
+const middleLogoPosition = new Vector3(logoPosition.x, logoPosition.y, -blackLayerDepth / 2)
+const backLogoPosition = new Vector3(logoPosition.x, logoPosition.y, -blackLayerDepth / 2 - whiteLayerDepth)
 
 const logoShapes = computed(() => {
   if (!svgData.value) return []
 
-  const shapes = []
-  const paths = svgData.value.paths
-
-  for (let i = 0; i < paths.length; i++) {
-    const path = paths[i]
-    const pathShapes = SVGLoader.createShapes(path)
-
-    for (let j = 0; j < pathShapes.length; j++) {
-      shapes.push(pathShapes[j])
-    }
-  }
-
-  return shapes
+  return svgData.value.paths.flatMap(path =>
+    SVGLoader.createShapes(path),
+  )
 })
 
-const coinGroupRef = ref()
+const frontLogoGeometries = computed(() =>
+  createLogoGeometries(whiteLayerDepth),
+)
+
+const middleLogoGeometries = computed(() =>
+  createLogoGeometries(blackLayerDepth),
+)
+
+const backLogoGeometries = computed(() =>
+  createLogoGeometries(whiteLayerDepth),
+)
+
+function createLogoGeometries(depth: number) {
+  return logoShapes.value.map(shape => new ExtrudeGeometry(shape, {
+    bevelEnabled: false,
+    curveSegments: 10,
+    depth,
+  }))
+}
+
+const logoGroupRef = ref()
 
 const { onBeforeRender } = useLoop()
 const preferredMotion = usePreferredReducedMotion()
 
 onBeforeRender(({ delta }) => {
-  if (!coinGroupRef.value) return
+  if (!logoGroupRef.value) return
   if (preferredMotion.value === 'reduce') return
-  coinGroupRef.value.rotation.y += delta
+  logoGroupRef.value.rotation.y += delta * 0.5
 })
 </script>
